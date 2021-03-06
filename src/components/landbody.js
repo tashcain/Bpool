296d74a4ff8443be67b3d8e2ca857b9435e0ba1f
Your Name <evreneducation@gmail.com>
2021-03-06 12:32:19 +0530

    deploychk

--- src/components/landbody.js
+++ src/components/landbody.js
@@ -15,22 +15,31 @@ function Landbody(){
             <Container>
                 <Row>
                     <Col data-aos="fade-right">
-                        <span className="heading" >Egalitarian Investing</span>
+                        <span className="heading" > LUCRATIVE INVESTING</span>
                         <br/>
                         <br/>
                         <span className="hunder" >
-                        <b>Bpool</b> believe all project stakeholders are as important as each other. Investment funds and communities should work side by side on projects, on the same terms, towards the same goals.
+                        <b>Bpool</b>        New Era of investment, incentives all small and big investors.
                         </span>
                         <br/>
                         <br/>
                         <span className="hunder" >
-                        <b>Bpool</b> harnesses their strengths, and aligns their incentives, so that the sum is greater than its constituent parts.
-                        </span>
+                        <b>BSCPOOL</b> is an innovative solution towards a sustainable farming model and built on smart chain where small investors can participate by paying minimal gas fee.
                         <br/>
+                        <b>BSCPOOL</b> is the best in making the safest profit strategies and mechanics where equal profit sharing is given the top priority. 
+                        our goal is to maximise stakeholders' profits and to constantly develop the protocol to be a competitive profit-making strategy and services deployment.
+                        
+
+                        <br/>
+                        <br/>
+                        <b>We are committed to bring innovative strategies and projects in coming time to give wings to our financial goals.</b>
+
+                        </span>
+                        
                         <br/>
                         <br/>
                         <b className="hunder" >
-                        Join us! We’re excited to start this journey together.
+                        Join us: we are excited to launch product for cummunity
                         </b>
                     </Col>
                     
